dc6e99aff4ce07d91aa67e48d829cf3640da7d9b
Collaborateur1-upgrade <61358939+Collaborateur1-upgrade@users.noreply.github.com>
2021-01-06 14:06:22 +0100

    Add files via upload
    save 2021/01/06 14h06

--- up_orderprocess/controllers/front/display.php
+++ up_orderprocess/controllers/front/display.php
@@ -5,53 +5,59 @@ class up_orderprocessdisplayModuleFrontController extends ModuleFrontController
     public function initContent()
     {
         parent::initContent();
-        
-        // Verification du premier formulaire
+
+        // Etape 3
         if (isset($_POST['step2'], $_POST['prestation']) && $_POST['prestation'] != 0){ // Si le deuxieme formulaire est envoyé
-            $this->setTemplate('module:up_orderprocess/views/templates/front/displaystep2.tpl');
 
-            // Listage des produits
-            $selectList = Product::getSimpleProducts($this->context->language->id);
-            $this->context->smarty->assign('selectList', $selectList);
+            print_r($_POST['prestation']);
+            $category = new Category($_POST['prestation']);
+            $categoryName = $category->getName($this->context->language->id);
+            print_r($categoryName);
+            print_r($_POST['produit']);
 
-            // Définition de la valeur séléctionné
-            $this->context->smarty->assign(
-                array(
-                    'produit' => '0'
-                )
-            );
+
+//            Code pour ajouter au panier, à utiliser avec la déclinaison trouvée grâce à id_produit (step1) et nom_déclinaison == nom_catégorie (step2) avec id_lang
+//            // On ajoute le produit au panier
+//            $Cart = $this->context->cart;
+//            $Cart->updateQty(1, $_POST['produit']);
+
+            // Tools::redirect('https://agissez-pour-vos-droits.fr/commande'); // Redirection sur le tunnel classique de Prestashop
+
+            // Etape 2
         }if (isset($_POST['step1'], $_POST['produit']) && $_POST['produit'] != 0){ // Si le premier formulaire est envoyé
-            $this->setTemplate('module:up_orderprocess/views/templates/front/displaystep1.tpl');
+        $this->setTemplate('module:up_orderprocess/views/templates/front/displaystep2.tpl');
 
-            $this->context->smarty->assign('suite', 'ok');
+        print_r($_POST['produit']);
 
-            // Listage des produits
-           $selectList = Product::getSimpleProducts($this->context->language->id);
-            $this->context->smarty->assign('selectList', $selectList);
+        // debug :
+        // $this->context->smarty->assign('suite', 'ok'); => mettre {$suite} dans .tpl
 
-            // Définition de la valeur séléctionné
-            $this->context->smarty->assign(
-                array(
+        // Listage des catégories
+        $selectList = Category::getHomeCategories($this->context->language->id);
+        $this->context->smarty->assign('selectList', $selectList);
+
+        // Assigner une valeur de base au select
+        $this->context->smarty->assign(
+            array(
+                'prestation' => '0',
+                'produit' => '$_POST["produit"]'
+            )
+        );
+
+        // Etape 1
+    }else{ // Si le premier formulaire n'est pas envoyé
+        $this->setTemplate('module:up_orderprocess/views/templates/front/displaystep1.tpl');
+
+        // Listage des produits
+        $selectList = Product::getSimpleProducts($this->context->language->id);
+        $this->context->smarty->assign('selectList', $selectList);
+
+        // Assigner une valeur de base au select
+        $this->context->smarty->assign(
+            array(
                 'produit' => '0'
-                )
-            );
-
-            $Cart = $this->context->cart;
-            $Cart->updateQty(1, $_POST['produit']);
-
-            Tools::redirect('https://agissez-pour-vos-droits.fr/commande');
-        }else{ // Si le premier formulaire n'est pas envoyé
-            $this->setTemplate('module:up_orderprocess/views/templates/front/display.tpl');
-
-            // Listage des catégories
-            $selectList = Category::getHomeCategories($this->context->language->id);
-            $this->context->smarty->assign('selectList', $selectList);
-            
-            $this->context->smarty->assign(
-                array(
-                'prestation' => '0'
-                )
-            );
-        }
+            )
+        );
+    }
     }
 }
